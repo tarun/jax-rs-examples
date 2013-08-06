39c7c70c38e4080dd902365e1f549e31ec9866a6
Tarun Kothuri <tkothuri@book.com>
2013-08-06 12:07:29 -0400

    saving new vendor

--- inventory-service/src/main/java/com/tarunbk/example/jaxrs/inventory/service/vendor/VendorResource.java
+++ inventory-service/src/main/java/com/tarunbk/example/jaxrs/inventory/service/vendor/VendorResource.java
@@ -3,6 +3,16 @@ package com.tarunbk.example.jaxrs.inventory.service.vendor;
 import static com.tarunbk.example.jaxrs.inventory.service.vendor.VendorsResource.VENDORS;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
+import static javax.ws.rs.core.Response.created;
+import static javax.ws.rs.core.Response.ok;
+import static javax.ws.rs.core.Response.serverError;
+import static javax.ws.rs.core.Response.status;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,6 +20,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import com.tarunbk.example.jaxrs.inventory.model.vendor.Vendor;
 
@@ -19,22 +30,34 @@ public class VendorResource {
     @GET
     @Path("/{vendorId}")
     @Produces({ APPLICATION_JSON, APPLICATION_XML })
-    public Vendor getVendor(@PathParam("vendorId") String vendorId) {
-        if (vendorId != null) {
-            return VENDORS.get(vendorId);
+    public Response getVendor(@PathParam("vendorId") String vendorId) {
+        if (vendorId != null && !vendorId.isEmpty()) {
+            Vendor vendor = VENDORS.get(vendorId);
+            if (vendor != null) {
+                return ok(vendor).build();
+            } else {
+                return status(NOT_FOUND).build();
+            }
+        } else {
+            return status(BAD_REQUEST).entity("Please provide a vendor id in the path").type(TEXT_PLAIN_TYPE).build();
         }
-        return null;
     }
 
     @POST
     @Consumes({ APPLICATION_JSON, APPLICATION_XML })
-    public Vendor createVendor(Vendor vendor) {
+    public Response createVendor(Vendor vendor) {
         if (vendor != null) {
             String id = String.valueOf(VENDORS.size() + 1);
             vendor.setId(id);
             VENDORS.put(id, vendor);
-            return vendor;
+            URI uri;
+            try {
+                uri = new URI("/vendor/" + id);
+                return created(uri).build();
+            } catch (URISyntaxException e) {
+                return serverError().build();
+            }
         }
-        return null;
+        return status(BAD_REQUEST).build();
     }
 }
